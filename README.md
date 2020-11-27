726c9ee87c3e9b6ac2f14ec51910124804a0d260
cp-sumi-k <sumita.k@canopas.com>
2020-11-27 16:14:40 +0530

    Update converter_test.go and README.md

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 ## JSONToStructConverter
 
-##### This repository gives you data of your choice from complex nested JSON in golang.
-##### Call ****StructuredJSON(JSONData, fieldName)**** and see the result.
+This repository gives you data of your choice from complex nested JSON in golang.
+
+Call ****StructuredJSON(JSONData, fieldName)**** and see the result.
 
 ### Example
 
 ````
 jsonData := "{\"data\" : {\"student\" : {\"name\" : \"abc\",\"std\" : \"8\",\"roll no\" : \"20\"}}}"
 
-StructuredJSON(jsonData, "data")
+StructuredJSON(jsonData, "student")
 
 Result : {"name":"abc","roll no":"20","std":"8"}
 ````
 
-##### We can unmarshal result to required struct.
+We can unmarshal result to required struct.
--- converter_test.go
+++ converter_test.go
@@ -16,17 +16,23 @@ var testCases = []struct {
 		"\"student\""},
 
 	{"nestedStudentNullData",
-		"{\"data\" : {\"student\" : {\"name\" : \"abc\",\"std\" : \"8\",\"roll no\" : \"20\",\"subjects\" : [{\"name\" : \"english\",\"tutor\" : \"xyz sir\"},{\"name\" : \"maths\",\"tutor\" : \"pqr madam\"}]}}}",
+		"{\"data\" : {\"student\" : {\"name\" : \"abc\",\"std\" : \"8\",\"roll no\" : \"20\"," +
+			"\"subjects\" : [{\"name\" : \"english\",\"tutor\" : \"xyz sir\"},{\"name\" : \"maths\",\"tutor\" : \"pqr madam\"}]}}}",
 		"nam",
 		"null"},
 
 	{"nestedStudentData",
-		"{\"data\" : {\"student\" : {\"name\" : \"abc\",\"std\" : \"8\",\"roll no\" : \"20\",\"subjects\" : [{\"name\" : \"english\",\"tutor\" : \"xyz sir\"},{\"name\" : \"maths\",\"tutor\" : \"pqr madam\"}]}}}",
+		"{\"data\" : {\"student\" : {\"name\" : \"abc\",\"std\" : \"8\",\"roll no\" : \"20\"," +
+			"\"subjects\" : [{\"name\" : \"english\",\"tutor\" : \"xyz sir\"},{\"name\" : \"maths\",\"tutor\" : \"pqr madam\"}]}}}",
 		"name",
 		"\"abc\""},
 
 	{"nestedRecipeData",
-		"{\"items\":{\"item\":[{\"id\": \"0001\",\"type\": \"donut\",\"name\": \"Cake\",\"ppu\": 0.55,\"batters\":{\"batter\":[{ \"id\": \"1001\", \"type\": \"Regular\" },{ \"id\": \"1002\", \"type\": \"Chocolate\" },{ \"id\": \"1003\", \"type\": \"Blueberry\" },{ \"id\": \"1004\", \"type\": \"Devil's Food\" }]},\"topping\":[{ \"id\": \"5001\", \"type\": \"None\" },{ \"id\": \"5002\", \"type\": \"Glazed\" },{ \"id\": \"5005\", \"type\": \"Sugar\" },{ \"id\": \"5007\", \"type\": \"Powdered Sugar\" },{ \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },{ \"id\": \"5003\", \"type\": \"Chocolate\" },{ \"id\": \"5004\", \"type\": \"Maple\" }]}]}}",
+		"{\"items\":{\"item\":[{\"id\": \"0001\",\"type\": \"donut\",\"name\": \"Cake\",\"ppu\": 0.55," +
+			"\"batters\":{\"batter\":[{ \"id\": \"1001\", \"type\": \"Regular\" },{ \"id\": \"1002\", \"type\": \"Chocolate\" },{ \"id\": \"1003\", \"type\": \"Blueberry\" },{ \"id\": \"1004\", \"type\": \"Devil's Food\" }]}," +
+			"\"topping\":[{ \"id\": \"5001\", \"type\": \"None\" },{ \"id\": \"5002\", \"type\": \"Glazed\" }," +
+			"{ \"id\": \"5005\", \"type\": \"Sugar\" },{ \"id\": \"5007\", \"type\": \"Powdered Sugar\" },{ \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" }," +
+			"{ \"id\": \"5003\", \"type\": \"Chocolate\" },{ \"id\": \"5004\", \"type\": \"Maple\" }]}]}}",
 		"batters",
 		"{\"batter\":[{\"id\":\"1001\",\"type\":\"Regular\"},{\"id\":\"1002\",\"type\":\"Chocolate\"},{\"id\":\"1003\",\"type\":\"Blueberry\"},{\"id\":\"1004\",\"type\":\"Devil's Food\"}]}"},
 }
